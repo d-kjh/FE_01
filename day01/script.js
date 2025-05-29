a92f3a03ade522b7fdc3009fd2a66aabb0b698bc
d-kjh <dkjh9942@gmail.com>
2025-05-29 12:14:17 +0900

    [docs]:css,js복습

--- day01/script.js
+++ day01/script.js
@@ -1,3 +1,4 @@
+//querySelector : JavaScript에서 HTML 요소를 선택할 때 사용하는 메서드
 const $screen = document.querySelector('#screen');
 const $toggleButton = document.querySelector('#button');
 
@@ -27,5 +28,27 @@ if ($screen && $toggleButton) {
   $toggleButton.addEventListener('click', () => {
     stopWatchOn = !stopWatchOn; //토글
     console.log('stopWatchOn: ', stopWatchOn);
+    $toggleButton.innerHTML = stopWatchOn ? '■' : '▶';
+    // $toggleButton.style.backgroundColor = stopWatchOn ? 'tomato' : 'steelblue';
+    $toggleButton.classList.toggle('stop-color');
+    if (stopWatchOn) {
+      // 첫번째 인자 : 콜백함수
+      // 두번째 인자 : Number(ms)
+      timeInterval = setInterval(() => {
+        /*
+        1초 > 100
+        1분 > 60초 > 6000
+        */
+        seconds++;
+        const mm = String(Math.floor(seconds / 6000) % 60).padStart(2, '0'); // 분
+        const ss = String(Math.floor(seconds / 100) % 60).padStart(2, '0'); // 초
+        const cs = String(seconds % 100).padStart(2, '0');
+        $screen.innerText = `${mm}:${ss}:${cs}`;
+      }, 10);
+    } else {
+      clearInterval(timeInterval);
+      seconds = 0;
+      $screen.innerText = '00:00:00';
+    }
   });
 }
