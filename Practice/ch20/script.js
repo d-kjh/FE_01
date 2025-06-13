97976dd8230877529e99a7027e53ac4ad0542328
d-kjh <dkjh9942@gmail.com>
2025-06-13 11:12:04 +0900

    얄코페이지

--- Practice/ch20/script.js
+++ Practice/ch20/script.js
@@ -89,3 +89,38 @@ $currLiList.forEach((item, idx) => {
   );
   item.addEventListener('mouseleave', () => ($currProgBar.style.width = 0));
 });
+
+const $contactTabs = document.querySelectorAll('#ct_1, #ct_2');
+console.log($contactTabs);
+const $contactSlideCon = document.querySelector('#contact .contact__slide-con');
+$contactTabs.forEach((item, idx) => {
+  const marginLeft = [0, '-100vw'][idx];
+  // 위 한 줄이랑 같다
+  // let marginLeft;
+  // if (idx === 0) {
+  //   marginLeft = 0;
+  // } else if (idx === 1) {
+  //   marginLeft = '-100vw';
+  // }
+  item.addEventListener('click', () => {
+    $contactSlideCon.style.marginLeft = marginLeft;
+  });
+});
+
+const $menuBtn = document.querySelector(
+  'header.header button.header__menu-btn'
+);
+const $headerNav = document.querySelector('header.header nav.header__nav');
+
+$menuBtn.addEventListener('click', (e) => {
+  $menuBtn.classList.toggle('on');
+  $headerNav.classList.toggle('active');
+
+  e.stopPropagation(); // 버블링 중지
+});
+
+const $body = document.querySelector('body');
+$body.addEventListener('click', () => {
+  $menuBtn.classList.remove('on');
+  $headerNav.classList.remove('active');
+});
